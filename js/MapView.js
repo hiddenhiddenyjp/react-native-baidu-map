002ecd4184e49c908029279d82b7cd90988161b1
hiddenhiddenyjp <1316430403@qq.com>
2017-09-18 19:22:30 +0800

    second test

--- js/MapView.js
+++ js/MapView.js
@@ -58,7 +58,7 @@ export default class MapView extends Component {
   }
 
   render() {
-    console.warn('9999');
+    console.warn('9998889');
     return <BaiduMapView {...this.props} onChange={this._onChange.bind(this)}/>;
   }
 }
